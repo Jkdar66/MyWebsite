12eea96870b1b08721536c0e5b28c10aaf37bd4c
jakdar jalalnaser <jakdarnaser@gmail.com>
2021-05-20 23:17:48 +0200

    Come on!

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ var navbar_btn = document.getElementById("btn-svg");
 var nav = document.getElementsByClassName("nav").item(0);
 
 navbar_btn.addEventListener("mousedown", (e)=>{
-    if(nav.style.marginLeft == "100%"){
-        nav.style.width = "100%";
-        nav.style.height = "100%";
-        nav.style.marginLeft = "0%";
-    }else {
-        nav.style.width = "0%";
-        nav.style.height = "0%";
-        nav.style.marginLeft = "100%";
+    if(!(nav.classList.contains("nav-animi"))){
+        nav.classList.add("nav-animi");
+    }else{
+        nav.classList.remove("nav-animi");
     }
 });
+
+window.onresize = (e)=>{
+    if(innerWidth > 1000){
+        if(nav.classList.contains("nav-animi")){
+            nav.classList.remove("nav-animi");
+        }
+    }
+}
+// https://stackoverflow.com/questions/49655095/how-to-update-my-cloned-repository-according-to-the-official-repository
