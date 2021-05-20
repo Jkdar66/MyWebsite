6b7a28a98c6f92f7cc7ffda9061591f6cf0003f0
jakdar jalalnaser <jakdarnaser@gmail.com>
2021-05-20 18:21:56 +0200

    BYE BYE

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// var navbar_btn = document.getElementById("nav-btn");
-// var nav = document.getElementsByClassName("nav").item(0);
+var navbar_btn = document.getElementById("btn-svg");
+var nav = document.getElementsByClassName("nav").item(0);
 
-// navbar_btn.addEventListener("mousedown", (e)=>{
-//     if(nav.style.marginLeft == "100%"){
-//         nav.style.width = "100%";
-//         nav.style.height = "100%";
-//         nav.style.marginLeft = "0%";
-//     }else {
-//         nav.style.width = "0%";
-//         nav.style.height = "0%";
-//         nav.style.marginLeft = "100%";
-//     }
-// });
+navbar_btn.addEventListener("mousedown", (e)=>{
+    if(nav.style.marginLeft == "100%"){
+        nav.style.width = "100%";
+        nav.style.height = "100%";
+        nav.style.marginLeft = "0%";
+    }else {
+        nav.style.width = "0%";
+        nav.style.height = "0%";
+        nav.style.marginLeft = "100%";
+    }
+});
